feaacb2e55cc78798618420502828260be90d623
Cade Chaney <cadechaney@live.com>
2023-01-11 18:54:09 -0600

    Finalized

--- friends.js
+++ friends.js
@@ -1,5 +1,7 @@
-var name = "Max";
+var name = "Molly";
 var otherName = "steve";
 if(name === "Max") {
     console.log("Hello Max")
+} else if (name !== "Max") {
+    console.log(`Nice to meet you ${otherName}`)
 }
